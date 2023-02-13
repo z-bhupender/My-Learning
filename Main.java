61b8e8759e2e68e3a8f99f4e630e9ea222d6d35a
z-bhupender <bhupender@zenarate.com>
2023-02-13 16:11:28 +0530

    Updated

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
-class Main{
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String s = "PRA12Ga782d45601$3";
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                sum += (s.charAt(i) - '0');
+            }
+        }
+        System.out.println(sum);
     }
 }
